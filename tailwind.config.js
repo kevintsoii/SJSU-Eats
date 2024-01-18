5c7d588ce40dd63cce1151b611fdd2ea90332903
kevintsoii <kevin.tsoi44@gmail.com>
2024-01-17 19:14:42 -0800

    add site theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#0055A2",
+        gold: "#E5A823",
+        background: "#E2E2E2",
+      },
+    },
   },
   plugins: [],
 };
