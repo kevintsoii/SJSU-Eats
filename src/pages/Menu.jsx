fd66078e6f4a57d3d10ae18d24a90ab49adca289
kevintsoii <kevin.tsoi44@gmail.com>
2024-01-19 00:15:04 -0800

    update animations

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "../components/Header";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default Menu;
