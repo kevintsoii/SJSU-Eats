1fed19606471d2c4f092ed9c02c4b8469640aab3
kevintsoii <kevin.tsoi44@gmail.com>
2024-01-19 00:14:43 -0800

    add responsiveness to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,52 @@
 import { Link } from "react-router-dom";
-import NavbarItem from "./navbar/NavbarItem";
+import Navbar from "./navbar/Navbar";
+import Menu from "./navbar/Menu";
+import { useEffect, useState } from "react";
+
+const pages = [
+  {
+    name: "Menu",
+    path: "/menu",
+  },
+  {
+    name: "Calorie Tracker",
+    path: "/tracker",
+  },
+];
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <header className="fixed left-0 right-0 flex items-center justify-between md:px-20 h-20 border-b border-black backdrop-blur-md">
-      <Link to="/" className="flex items-center">
-        <img src="/logo.png" alt="logo" className="w-14 h-14" />
-        <h1 className="text-4xl font-semibold">
-          <span className="text-blue">SJSU</span>Eats
-        </h1>
-      </Link>
-      <nav>
-        <ul className="flex items-center font-medium">
-          <NavbarItem to="/menu" text="Menu" />
-          <NavbarItem to="/tracker" text="Calorie Tracker" />
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="fixed left-0 right-0 flex items-center justify-between shadow-lg h-20 border-b border-black backdrop-blur-md px-5 md:px-20">
+        <Link to="/" className="flex items-center">
+          <img src="/logo.png" alt="logo" className="w-14 h-14" />
+          <h1 className="text-4xl font-semibold">
+            <span className="text-blue">SJSU</span>Eats
+          </h1>
+        </Link>
+        <Navbar
+          screenWidth={screenWidth}
+          pages={{ ...pages }}
+          onToggle={toggleMenu}
+        />
+      </header>
+      {screenWidth < 640 && showMenu && <Menu pages={{ ...pages }} />}
+    </>
   );
 };
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import NavbarItem from "./NavbarItem";
+
+const Navbar = ({ screenWidth, pages, onToggle }) => {
+  return (
+    <nav className="flex">
+      {screenWidth >= 640 ? (
+        <ul className="hidden md:flex items-center font-medium">
+          {Object.keys(pages).map((key) => (
+            <NavbarItem
+              key={key}
+              to={pages[key].path}
+              text={pages[key].name}
+              classes="rounded-full hover:bg-gold"
+            />
+          ))}
+        </ul>
+      ) : (
+        <button onClick={onToggle} className="">
+          <img src="/menu.svg" className="w-10"></img>
+        </button>
+      )}
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  screenWidth: PropTypes.number,
+  pages: PropTypes.shape({
+    name: PropTypes.string,
+    path: PropTypes.string,
+  }),
+  onToggle: PropTypes.func,
+};
+
+export default Navbar;
