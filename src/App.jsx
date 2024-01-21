62f292e269bfed5723377ad46f6621477c4948bc
kevintsoii <kevin.tsoi44@gmail.com>
2024-01-21 13:48:03 -0800

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Tracker from "./pages/Tracker";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/tracker" element={<Tracker />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/tracker" element={<Tracker />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
