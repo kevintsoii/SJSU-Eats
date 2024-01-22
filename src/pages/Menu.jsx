ef1023b5a61350c746878159e90109356894dfad
kevintsoii <kevin.tsoi44@gmail.com>
2024-01-22 01:42:26 -0800

    update loadng and error components

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,12 @@
-import Header from "../components/Header";
-import MealButton from "../components/menu/MealButton";
+import moment from "moment";
 import { useState } from "react";
 import { useQuery } from "react-query";
-import moment from "moment";
+
+import Header from "../components/Header";
+import MealButton from "../components/menus/MealButton";
+import Menus from "../components/menus/Menus";
+import Error from "../components/status/Error";
+import Loading from "../components/status/Loading";
 
 const convrtDateToMeal = (date) => {
   const hour = date.hour();
@@ -22,11 +26,13 @@ const Menu = () => {
   const [date, setDate] = useState(moment());
   const [meal, setMeal] = useState(convrtDateToMeal(date));
 
-  const { data, loading, error } = useQuery(
+  const { isLoading, error, data } = useQuery(
     ["menus", date.format("YYYY-MM-DD")],
     () => fetchData(date.format("YYYY-MM-DD")),
     {
       staleTime: Infinity,
+      retry: 0,
+      refetchOnWindowFocus: false,
     }
   );
 
@@ -37,11 +43,13 @@ const Menu = () => {
   return (
     <>
       <Header />
+
       <div className="mt-20 text-center">
         <h1 className="text-lg sm:text-3xl pt-10">
           {date.format("dddd, MMMM DD")}
         </h1>
       </div>
+
       <div className="meal-selector flex justify-center mt-4">
         <div className="flex justify-between rounded sm:border sm:border-black sm:text-lg min-w-72 overflow-hidden">
           {["Breakfast", "Lunch", "Dinner"].map((m) => (
@@ -53,6 +61,10 @@ const Menu = () => {
             />
           ))}
         </div>
+      </div>
+
+      <div className="flex justify-center pt-8">
+        {error ? <Error /> : isLoading ? <Loading /> : <Menus />}
       </div>
     </>
   );
