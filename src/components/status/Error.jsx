655bdd89f1b4c7e1fe4c3d7004cfbcee0a777185
kevintsoii <kevin.tsoi44@gmail.com>
2024-02-01 23:13:13 -0800

    fix google image scraping

--- src/components/status/Error.jsx
+++ src/components/status/Error.jsx
@@ -1,10 +1,20 @@
-const Error = () => {
+import PropTypes from "prop-types";
+
+const Error = ({ message }) => {
   return (
     <div className="flex flex-col items-center justify-items-center bg-[#d97171] rounded-lg w-[60%] lg:w-[30%] py-5 fade-down">
       <img src="./error.png" className="w-36" />
-      <h3 className="text-2xl py-3">An error occurred</h3>
+      <h3 className="text-2xl py-3">
+        {message === "No menus found for this date."
+          ? "The Dining Commons is closed."
+          : "An error occurred"}
+      </h3>
     </div>
   );
+};
+
+Error.propTypes = {
+  message: PropTypes.string,
 };
 
 export default Error;
