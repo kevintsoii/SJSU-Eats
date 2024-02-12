ec702571d78859e49325288b3161a9cb9799193e
kevintsoii <kevin.tsoi44@gmail.com>
2024-02-11 22:51:57 -0800

    remove unneeded redux logic

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,9 +1,22 @@
 import Header from "../components/Header";
 
+import { add, remove, removeAll, selectItems } from "../features/itemsSlice";
+import { useDispatch, useSelector } from "react-redux";
+
 const Tracker = () => {
+  const items = useSelector(selectItems);
+
+  console.log(items);
+
   return (
     <>
       <Header />
+
+      <span className="block mt-20"></span>
+
+      <div>
+        <h1 className="text-5xl">Track your Nutrients!</h1>
+      </div>
     </>
   );
 };
