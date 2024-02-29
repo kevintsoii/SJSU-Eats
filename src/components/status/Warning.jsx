d6f7ccfb3cf2ef80ed706f60c559e1084efe4fb9
kevintsoii <kevin.tsoi44@gmail.com>
2024-02-28 17:42:46 -0800

    update warning

--- src/components/status/Warning.jsx
+++ src/components/status/Warning.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Warning = ({ message }) => {
   return (
-    <div className="flex flex-col items-center justify-items-center bg-[#cfd45b] rounded-lg w-[60%] lg:w-[30%] mt-6 py-5 fade-down">
+    <div className="flex flex-col items-center justify-items-center bg-[#ced450] rounded-lg w-[60%] lg:w-[30%] mt-6 py-5 fade-down">
       <img src="./warning.svg" className="w-36" />
       <h3 className="text-2xl py-3">{message}</h3>
     </div>
