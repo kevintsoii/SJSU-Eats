2491b0dba4046990c4ab8a142732d34ca66f3fb1
kevintsoii <kevin.tsoi44@gmail.com>
2024-03-24 18:46:18 -0700

    rework statuses for mobile

--- src/components/status/Error.jsx
+++ src/components/status/Error.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Error = ({ message }) => {
   return (
-    <div className="flex flex-col items-center justify-items-center bg-[#d97171] rounded-lg w-[60%] lg:w-[30%] mt-6 py-5 fade-down">
+    <div className="flex flex-col items-center justify-items-center bg-[#d97171] rounded-lg w-[80%] md:w-[60%] lg:w-[30%] mt-6 py-5 fade-down">
       <img src="./error.png" className="w-36" />
       <h3 className="text-2xl py-3">
         {message === "No menus found for this date."
