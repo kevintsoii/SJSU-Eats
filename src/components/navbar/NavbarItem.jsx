1067fca5efa2f41c0ffbab85188af44b6d10b20d
kevintsoii <kevin.tsoi44@gmail.com>
2024-04-05 20:28:37 -0700

    add item count to large screen navbar

--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -1,15 +1,31 @@
 import PropTypes from "prop-types";
 
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectSize } from "../../features/itemsSlice";
 
 const NavbarItem = ({ to, text, classes }) => {
+  const itemsSize = useSelector(selectSize);
+
   return (
-    <Link
-      to={to}
-      className={`text-xl flex py-1 px-3 md:hover:bg-gold w-full ${classes}`}
-    >
-      {text}
-    </Link>
+    <div className="relative">
+      <Link
+        to={to}
+        className={`text-xl flex py-1 px-3 md:hover:bg-gold w-full ${classes}`}
+      >
+        {text}
+      </Link>
+      {to === "/tracker" && itemsSize > 0 && (
+        <h2
+          className={`hidden absolute sm:flex top-0 right-[-12px] ${
+            itemsSize > 9 ? "-mr-1" : ""
+          } rounded-full bg-blue/40 px-1.5`}
+        >
+          {itemsSize}
+        </h2>
+      )}
+    </div>
   );
 };
 
