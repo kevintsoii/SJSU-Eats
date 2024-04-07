75cd60916a8fe9409ea5fa292cec53834b9ea943
kevintsoii <kevin.tsoi44@gmail.com>
2024-04-07 00:32:33 -0700

    fix status spacing

--- src/components/status/Warning.jsx
+++ src/components/status/Warning.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Warning = ({ message }) => {
   return (
-    <div className="flex flex-col items-center justify-items-center bg-[#e6a82e] rounded-lg w-[80%] md:w-[60%] lg:w-[30%] mt-6 py-5 fade-down">
+    <div className="flex flex-col items-center justify-items-center bg-[#e6a82e] rounded-lg w-[80%] md:w-[60%] lg:w-[30%] px-2 mt-6 py-5 fade-down">
       <img src="./warning.svg" className="w-36" />
       <h3 className="text-2xl py-3">{message}</h3>
     </div>
